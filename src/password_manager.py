d6075e130c7540a3b87d9aa6eb5c37023fb0c3b3
infoomics <sbx_94@hotmail.com>
2023-03-15 11:59:49 +0000

    add try..except to create_secret()

--- src/password_manager.py
+++ src/password_manager.py
@@ -4,18 +4,24 @@ import json
 
 client = boto3.client('secretsmanager')
 
+
 def create_secret():
     secret_id = 'Missile_Launch_Codes2'
     user_id = 'bidenj'
     password = 'pa55word'
     secret_json = { 'user_id': user_id, 'password': password }
 
-    response = client.create_secret(
-        Name=secret_id,
-        SecretString=json.dumps(secret_json)
-    )
+    try:
+        response = client.create_secret(
+            Name=secret_id,
+            SecretString=json.dumps(secret_json)
+        )
 
-    return response
+        return response
+    except Exception as e:
+        print(e)
+        
+        return 'An unexpected error occured.'
 
 # response = create_secret()
 # print(response)
