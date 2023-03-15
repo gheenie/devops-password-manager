f27ad84de83ee58ee8b5ef5873fa907e643a1018
infoomics <sbx_94@hotmail.com>
2023-03-15 11:26:33 +0000

    add create_secret(); non-dynamic inputs

---
+++ src/password_manager.py
@@ -0,0 +1,21 @@
+import boto3
+import json
+
+
+client = boto3.client('secretsmanager')
+
+def create_secret():
+    secret_id = 'Missile_Launch_Codes2'
+    user_id = 'bidenj'
+    password = 'pa55word'
+    secret_json = { 'user_id': user_id, 'password': password }
+
+    response = client.create_secret(
+        Name=secret_id,
+        SecretString=json.dumps(secret_json)
+    )
+
+    return response
+
+# response = create_secret()
+# print(response)
