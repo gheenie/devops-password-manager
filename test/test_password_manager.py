1e9f90a404d3c815f828a0f5b2c6fd301e8dff03
infoomics <sbx_94@hotmail.com>
2023-03-15 17:03:24 +0000

    change test param to fixture and remove duplicate code

--- test/test_password_manager.py
+++ test/test_password_manager.py
@@ -27,12 +27,10 @@ def premock_secretsmanager(aws_credentials):
 def precreate_secret(premock_secretsmanager):
     patch_client(secrets_manager)
 
-    create_secret()
-
-
-def test_valid_secrets_successfully_stores_in_secretsmanager(premock_secretsmanager):
-    patch_client(secrets_manager)
-
     response = create_secret()
-    
-    assert response['ARN'].startswith('arn:aws') == True
+
+    return response
+
+
+def test_valid_secrets_successfully_stores_in_secretsmanager(precreate_secret):
+    assert precreate_secret['ARN'].startswith('arn:aws') == True
