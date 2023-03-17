50330c0d0ea168940bda8e68cbeee4a060aec9ad
infoomics <sbx_94@hotmail.com>
2023-03-17 13:55:03 +0000

    add first working code for retrieve_secret()

--- src/password_manager.py
+++ src/password_manager.py
@@ -1,15 +1,16 @@
 import boto3
 import json
 from botocore.exceptions import ClientError
+import os
 
 
 secrets_manager = boto3.client('secretsmanager')
 
 
 def create_secret(secret_id = 'Missile_Launch_Codes2', user_id = 'bidenj', password = 'pa55word'):
-    secret_json = { 'user_id': user_id, 'password': password }
-
     try:
+        secret_json = { 'user_id': user_id, 'password': password }
+
         secrets_manager.create_secret(
             Name=secret_id,
             SecretString=json.dumps(secret_json)
@@ -38,6 +39,24 @@ def list_secrets():
             message += '\n' + secret['Name']
 
         return message
+    except Exception as error:
+        print(error)
+        
+        return 'An unexpected error occured.'
+
+
+def retrieve_secret(secret_id):
+    try:
+        output_filename = 'secret.txt'
+        output_path = f'data/{output_filename}'
+        
+        secret_string = secrets_manager.get_secret_value(SecretId=secret_id)['SecretString']
+
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        with open(output_path, 'w', encoding='utf-8') as file:
+            file.write(secret_string)
+
+        return f'Secrets stored in local file {output_filename}'
     except Exception as error:
         print(error)
         
